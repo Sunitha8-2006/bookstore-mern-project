76a833d34a4780775387dd079deddca8cf27deb6
Sunitha8-2006 <bujjiappanapalli@gmail.com>
2026-07-12 18:38:51 +0530

    https://bookstore-backend-n33a.onrender.com/api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bookstore-backend-n33a.onrender.com",
+  baseURL: "https://bookstore-backend-n33a.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
