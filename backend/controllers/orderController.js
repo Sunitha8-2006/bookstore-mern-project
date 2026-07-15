484d0beda39a8131f6b089c5f153114c0a32e84a
Sunitha8-2006 <bujjiappanapalli@gmail.com>
2026-07-15 20:06:26 +0530

    Fix order controller functions

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,47 @@
+const Order = require("../models/Order");
+
+// Create Order
+const createOrder = async (req, res) => {
+  try {
+    const order = new Order({
+      ...req.body,
+      user: req.user._id,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get logged-in user's orders
+const getMyOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get all orders
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({})
+      .populate("user", "name email")
+      .sort({ createdAt: -1 });
+
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Update Order Status
 const updateOrderStatus = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id);
@@ -9,7 +53,6 @@ const updateOrderStatus = async (req, res) => {
     order.status = req.body.status;
 
     const updatedOrder = await order.save();
-
     res.json(updatedOrder);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -21,4 +64,4 @@ module.exports = {
   getMyOrders,
   getAllOrders,
   updateOrderStatus,
-};
+}
